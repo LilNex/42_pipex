6d499e34b5e0c94cbd5a12f7c00a409ff6e2fba5
LilNex <nextorchq@gmail.com>
2023-03-26 17:43:55 +0000

    sync

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: ichaiq <ichaiq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 14:29:11 by ichaiq            #+#    #+#             */
-/*   Updated: 2023/03/26 16:58:46 by ichaiq           ###   ########.fr       */
+/*   Updated: 2023/03/26 17:41:49 by ichaiq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,20 @@ void ft_cleanexit(t_piputils *u)
 {
 	t_list		*tmp;
 	t_command	*cmd;
+	void 		*t;
 
 	tmp = u->commands;
 	while (tmp)
 	{
 		cmd = (t_command *)tmp->content;
 		free(cmd->fullpath);
+		t = cmd->args;
 		while (*cmd->args)
-		{
-			printf("arg : %s\n",*cmd->args);
 			free(*cmd->args++);
-		}
 		free(cmd->cmd);
+		free(cmd);
+		free(t);
+		free(tmp);
 		tmp = tmp->next;
 	}
 	ft_lstiter(u->paths, free);
@@ -43,11 +45,11 @@ int	main(int ac, char **av, char **envp)
 	if (ac >= 5)
 	{
 		utils = (t_piputils *) ft_calloc(1, sizeof(t_piputils));
-		
+
 		utils->envp = envp;
 		count = 0;
 		parse_path(envp, utils);
-		
+
 		parse_args(utils, ac, av);
 		if (utils->commands)
 			execute_commands(utils);
@@ -55,8 +57,6 @@ int	main(int ac, char **av, char **envp)
 		ft_cleanexit(utils);
 		while (1)
 			;
-		
-
 	}
 
 	return (0);
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: ichaiq <ichaiq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 18:55:07 by ichaiq            #+#    #+#             */
-/*   Updated: 2023/03/26 16:48:31 by ichaiq           ###   ########.fr       */
+/*   Updated: 2023/03/26 17:42:54 by ichaiq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,8 +55,8 @@ void	exec_cmd(int *inputfd, t_list *node, int *first, t_piputils *u)
 			dup2(u->outfilefd, STDOUT_FILENO);
 		execve(((t_command *)node->content)->fullpath,
 			((t_command *)node->content)->args, u->envp);
-		free(((t_command *)node->content)->fullpath);
-		free(((t_command *)node->content)->args);
+		// free(((t_command *)node->content)->fullpath);
+		// free(((t_command *)node->content)->args);
 	}
 	else
 		do_wait(first, newfd, inputfd, pid);
